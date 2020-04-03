9cd91c1a8041a3283985b4c4637abad52fc6dc15
Tomek <tomm.wrobel@gmail.com>
2020-04-03 22:54:05 +0200

    Added waiting class

---
+++ src/test/java/com/tom/wait/WaitForElement.java
@@ -0,0 +1,22 @@
+package com.tom.wait;
+
+import com.tom.driver.manager.DriverManager;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitForElement {
+    private static WebDriverWait getWebDriverWait() {
+        return new WebDriverWait(DriverManager.getWebDriver(), 10);
+    }
+
+    public static void waitUntilElementIsVisible(WebElement element) {
+        WebDriverWait webDriverWait = getWebDriverWait();
+        webDriverWait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public static void waitUntilElementIsClickable(WebElement element){
+        WebDriverWait webDriverWait = getWebDriverWait();
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+}
